065b3ae2342f0d3f39ee3f63e94167277dfb0aec
yn-jee <nayj2842@gmail.com>
2023-11-22 01:48:38 +0900

    fourth commit - master

--- myBC.py
+++ myBC.py
@@ -1,10 +1,13 @@
+import block as bl
 import fullnode as fn
 import usernode as un
 import random as rd
-import multiprocessing
+import ecdsa as ec
+from hashlib import sha256
+import multiprocessing as mp
 
 class myBC(fn.full_node, un.user_node):
-    def initialize(self, full_num, user_num, full_links_num):    # number of full/user nodes, number of full-full links
+    def __init__(self, full_num, user_num, full_links_num, genesis):    # number of full/user nodes, number of full-full links
 
         #----random topology 구성하기----
 
@@ -12,75 +15,124 @@ class myBC(fn.full_node, un.user_node):
         rd.shuffle(shuffled_index)
 
         # full-full link를 저장한 행렬
-        matrix_full = [[0 for _ in range(full_num)] for _ in range(full_num)]
+        self.matrix_full = [[0 for _ in range(full_num)] for _ in range(full_num)]
         # user-full link는 list_user[i]와 list_full[i]의 연결로 한다. 즉, 추가적으로 저장할 필요 없음.
 
         # matrix_full 초기화하기
         for i in range(full_num - 1):
             # 주대각선 초기화
-            matrix_full[i][i] = 1
+            self.matrix_full[i][i] = 1
             # 섞어 만든 리스트에서, 앞뒤로 이어진 노드들 연결하기(행렬에서 1로 표시)
-            matrix_full[shuffled_index[i]][shuffled_index[i + 1]] = 1
+            self.matrix_full[shuffled_index[i]][shuffled_index[i + 1]] = 1
             # 대칭행렬로 만들기
-            matrix_full[shuffled_index[i + 1]][shuffled_index[i]] = 1
+            self.matrix_full[shuffled_index[i + 1]][shuffled_index[i]] = 1
         # 주대각선 초기화 - 마지막 노드.
-        matrix_full[full_num - 1][full_num - 1] = 1
+        self.matrix_full[full_num - 1][full_num - 1] = 1
         # 0번째 원소와 full_num번째 원소 잇기
-        matrix_full[shuffled_index[0]][shuffled_index[full_num - 1]] = 1
-        matrix_full[shuffled_index[full_num - 1]][shuffled_index[0]] = 1
+        self.matrix_full[shuffled_index[0]][shuffled_index[full_num - 1]] = 1
+        self.matrix_full[shuffled_index[full_num - 1]][shuffled_index[0]] = 1
 
         # 추가 링크 연결, 이미 연결되어 있던 링크라면 다시 고르기.
         i = 0
         extra_link = full_links_num - full_num
         while i < extra_link:
             temp_link = rd.sample(shuffled_index, 2)
-            if matrix_full[temp_link[0]][temp_link[1]] == 0:
+            if self.matrix_full[temp_link[0]][temp_link[1]] == 0:
                 i = i + 1
-                matrix_full[temp_link[0]][temp_link[1]] = 1
-                matrix_full[temp_link[1]][temp_link[0]] = 1
+                self.matrix_full[temp_link[0]][temp_link[1]] = 1
+                self.matrix_full[temp_link[1]][temp_link[0]] = 1
 
-        #----노드 생성----
+
+
+        # ----노드 생성----
 
         # 이 모듈이 직접 실행될 때에만 다음 코드를 실행
-        if __name__ == "__main__" :
-            # 노드 잇기 - 두 노드 간 공유하는 스택들로써.
-            m = multiprocessing.Manager()
+        if __name__ == "__main__":
+            print("hi")
+
+            # 노드 잇기 - 두 노드 간 공유하는 스택 및 큐로써.
+            m = mp.Manager()
 
             # stack_matrix_block[i][j] :
-            # "A stack" shared between node_i, node_j
-            # contains messages from node_j to node_i propagating mined blocks
-            # so that node_i can search all stack_matrix_block[i][*] before every hash operation
+            # "A stack" shared between full_node_i, full_node_j
+            # contains messages from full_node_j to full_node_i propagating mined blocks
+            # so that full_node_i can search all stack_matrix_block[i][*] before every hash operation
             stack_matrix_block = [[m.list() for _ in range(full_num)] for _ in range(full_num)]
             for i in range(full_num):
                 for j in range(full_num):
-                    if matrix_full[i][j] != 1:
+                    if self.matrix_full[i][j] != 1:
                         # matrix_full에서 해당하는 위치가 1이 아니라면 stack 삭제
                         stack_matrix_block[i][j] = None
                     """else:
                         stack_matrix_block[i][j].append("hi")"""
 
             # stack_matrix_tx[i][j] :
-            # "A stack" shared between node_i, node_j
-            # contains messages from node_j to node_i propagating valid transactions
-            # so that node_i can search all stack_matrix_tx[i][*] before every block building
+            # "A stack" shared between full_node_i, full_node_j
+            # contains messages from full_node_j to full_node_i propagating valid transactions
+            # so that full_node_i can search all stack_matrix_tx[i][*] before every block building
             stack_matrix_tx = [[m.list() for _ in range(full_num)] for _ in range(full_num)]
             for i in range(full_num):
                 for j in range(full_num):
-                    if matrix_full[i][j] != 1:
+                    if self.matrix_full[i][j] != 1:
                         # matrix_full에서 해당하는 위치가 1이 아니라면 stack 삭제
                         stack_matrix_tx[i][j] = None
                     """else:
                         stack_matrix_tx[i][j].append("hi")"""
 
+            # queue_user[i] :
+            # "A queue" shared between user_node_i, full_node_i
+            # This queue is used when user_node_i generates a transaction and sends it to full_node_i
+            queue_user = [m.Queue() for _ in range(user_num)]
+
             list_full = []
             list_user = []
 
+            # 트랜잭션 상의 거래 기록을 각 노드가 알아야 함
+            # myBC 상의 거래와는 별개로, 각 사용자 노드가 자신이 소유한 자동차를 추적하고 있기 위해서 stack_matrix_user 노드가 필요함
+            # 각 사용자는 자동차를 판매한 뒤, 그 자동차의 구매자의 stack에 해당 트랜잭션의 정보를 공유함
+
+            stack_matrix_user = [[m.list() for _ in range(user_num)] for _ in range(user_num)]
+
+            #----full node 생성----
+            # 자신의 노드 번호 i, stack_matrix_block, stack_matrix_tx, queue_user[i]를 인자로 받음
             for i in range(full_num):
-                list_full.append(fn.full_node(i, stack_matrix_block, stack_matrix_tx))
+                if i < user_num:
+                    temp_full = fn.full_node(i, stack_matrix_block, stack_matrix_tx, queue_user[i])
+                    list_full.append(temp_full)
+                else:
+                    # __init__ 형식을 맞추기 위해 m.Queue()를 전달
+                    temp_full = fn.full_node(i, stack_matrix_block, stack_matrix_tx, m.Queue())
+                    list_full.append(temp_full)
+                list_full[i] = mp.Process(target=list_full[i].run_full, args=(genesis, ))
+                list_full[i].start()
 
-            print("set")
+            for i in range(full_num):
+                list_full[i].join()
+
+            #----user node 생성----
+
+            # 다른 user node의 vk 정보도 전달받음 ---- 일차원 배열 other_users로.
+            # 프로세스 간에 키를 전달하기 위해 string 형태로 변환한 뒤 전달
+            # 각 객체 생성 시 string 형태를 다시 키로 변환하여 사용
+
+            other_users = ["" for _ in range(user_num)]
+
+            for i in range(user_num):
+
+                temp_user = un.user_node(i, queue_user[i], stack_matrix_user, other_users)
+                list_user.append(temp_user)
+
+                list_user[i] = mp.Process(target=list_user[i].run_user, args=("", ""))
+                list_user[i].start()
+
+            for i in range(user_num):
+                list_user[i].join()
+
+            print("end")
 
 
-# 다음은 main.py에서 실행해야 하는 것들
-newbc = myBC
-newbc.initialize(newbc,7, 4, 10)
+
+
+genesis = bl.block([])
+
+newbc = myBC(7, 4, 10, genesis)
--- usernode.py
+++ usernode.py
@@ -2,27 +2,92 @@ from multiprocessing import Process
 import transaction as tx
 import random as rd
 import ecdsa as ec
+from hashlib import sha256
 class user_node(tx.transaction):
-    def __init__(self, node_num = 0):
+    def __init__(self, node_num, queue_user, stack_matrix_user, other_users):
         super().__init__()
+
+        self.node_num = node_num
+
         #----비대칭키 생성----
         # sk: signing key(private key)
         # vk: verifying key(public key)
         self.sk = ec.SigningKey.generate()
         self.vk = self.sk.verifying_key
 
+        # 프로세스 실행 시 pickle 오류 방지를 위한 형태 변환
+        self.sk = self.sk.to_string()
+        self.vk = self.vk.to_string()
+
+        # send_queue로 네트워크에 트랜잭션 전파
+        self.send_queue = queue_user
+
+        # 사용자끼리의 거래 기록을 stack_matrix_user에 저장
+        # stack_matrix_user i번째 열의 원소들을 자신의 send_stack_tx에 저장
+        # 여기에 자신이 판매자인 transaction을 공유
+        self.send_stack_tx = []
+        for row in stack_matrix_user:
+            self.send_stack_tx.append(row[self.node_num])
+
+        # stack_matrix_user i번째 행을 자신의 send_stack_tx에 저장
+        # 여기서 자신이 구매자인 transaction을 공유받음
+        self.recv_stack_tx = stack_matrix_user[self.node_num]
+
+        self.other_users = other_users
+
         #----무작위 개수만큼 자동차 생성----
         # 자동차 개수 설정
         self.cars_num = rd.randint(3, 11)
         # 자동차 개수만큼 자동차 생성 및 저장
-        self.cars = []
-        self.txs = []
+        # self.cars = [] ... 필요 없을 것 같음
+        # self.txs = []도 필요없음. recv_stack_tx에 저장됨
+#        self.run_user("", "")
+
+
+    def run_user(self, txid, sig):
+        # 처음 실행할 때
+        # user node가 소유한 자동차 별로 트랜잭션을 생성하고, full node와 공유하는 queue에 put
         for i in range(self.cars_num):
             car = tx.vehicle()
             Vid, modelName, manufacturedTime = car.get_vehicle_data()
-            new_tx = tx.transaction(Vid, modelName, manufacturedTime)
-            self.cars.append(car)
-            self.txs.append(new_tx)
 
-new_user = user_node()
-print()
+            new_tx_in = tx.tx_in(txid, sig)
+            new_tx_out = tx.tx_out(self.vk, Vid, modelName, manufacturedTime)
+            new_tx = tx.transaction(new_tx_in, new_tx_out)
+            self.recv_stack_tx[0].append(new_tx)    # 첫번째 recv 배열에 자신이 가진 차들을 저장
+
+        # 그 이후
+        # 약 15초 간격으로 트랜잭션 발생
+        # 자신이 관리하는 자동차 라면 자신이 그 구매 트랜잭션을 알고 있어야. ...
+
+        # while True:
+        #i = 0
+        #while i < 10: # 임시로 10번만
+        """if i == 2:
+                print()"""
+        for i in range(len(self.recv_stack_tx)):    # 자기가 트랜잭션 받을 배열들의 개수만큼
+            # 트랜잭션 생성 후 랜덤한 사용자에게 보내기
+            count = 0
+            while self.recv_stack_tx[i]:
+                count = count + 1
+                #print(str(count) + ' ' + str(self.node_num))
+                top_of_stack = self.recv_stack_tx[i].pop() # 자신이 받은 트랜잭션이라면 output이 자신의 vk
+                tx_out = top_of_stack.vout  # vout이 tx를 보낼 대상?
+
+            if tx_out.vk != "":
+                #if tx_out.vk.verify(top_of_stack.vin.sig, """여기에 메세지"""):   # 트랜잭션 전파 전 직접 검증해 보기
+                tx_to_redeem = top_of_stack.tx_id  # redeem 대상
+                print('.')
+        print("out of loop")
+
+
+
+
+
+
+
+
+    def put_queue(self, tx, queue):
+        queue.put(tx)
+
+#print()
